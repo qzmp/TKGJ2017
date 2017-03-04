6f0409442adc6f385ad2b79f574e36055b24c209
qzmp <lukasz.golebiowski@mail.ru>
2017-03-04 10:42:44 +0100

    some starting assets

---
+++ Assets/Character/Lumberjack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lumberjack : MonoBehaviour {
+
+    private int layerMask = 7; //growingTree
+    private Animator anim;
+    private bool cutting = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(nearTree())
+        {
+            if(!cutting)
+            {
+                cutting = true;
+                anim.SetBool("nearTree", true);
+            }
+        }
+        else
+        {
+            if(cutting)
+            {
+                cutting = false;
+                anim.SetBool("nearTree", false);
+            }
+            goToTree();
+        }
+	}
+
+    bool nearTree()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        return Physics.Raycast(transform.position, fwd, 1, layerMask);
+    }
+
+    void goToTree()
+    {
+        //TODO
+    }
+}
---
+++ Assets/Trees/TreeBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehaviour : MonoBehaviour
+{
+
+    public int maxHp;
+    private int hp;
+
+    // Use this for initialization
+    void Start()
+    {
+        hp = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            cutDown();
+        }
+    }
+    void cutDown()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.layer = 5;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Axe")
+        {
+            hp--;
+        }
+    }
+}
+
