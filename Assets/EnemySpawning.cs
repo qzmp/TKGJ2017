bd742fed05e53035c817e6a053fb5b5ee2224b57
qzmp <lukasz.golebiowski@mail.ru>
2017-03-04 21:05:31 +0100

    qwer

---
+++ Assets/EnemySpawning.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour {
+
+    public float spawnFrequency;
+    public GameObject enemyPrefab;
+    public bool active;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Random.value < spawnFrequency)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        }
+	}
+}
