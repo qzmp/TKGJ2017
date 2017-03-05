58fa32995c0412fe24f5f343591dea9036556f09
qzmp <lukasz.golebiowski@mail.ru>
2017-03-05 17:29:05 +0100

    final3

--- Assets/Weapons/WaspNest.cs
+++ Assets/Weapons/WaspNest.cs
@@ -23,7 +23,7 @@ public class WaspNest : MonoBehaviour {
             transform.parent = null;
             GetComponent<Rigidbody>().isKinematic = false;
             GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
-            gameObject.layer = 0;
+            gameObject.layer = 14;
         }
     }
 
